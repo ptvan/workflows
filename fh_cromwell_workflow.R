df050b38364c6d1667ecf234a4e869e2d0a69a72
Phu T. Van <pvan@fredhutch.org>
2020-02-18 09:42:52 -0800

    renamed and added to fh_cromwell_workflow.R

---
+++ fh_cromwell_workflow.R
@@ -0,0 +1,12 @@
+# 1. Set up MariaDB on gizmo: request a MariaDB instance at https://mydb.fredhutch.org/login
+# note the settings generated, eg. `mysql --host mydb --port 32217 --user pvan --password`
+#
+# 2. Clone the Cromwell server skeleton: 
+# `git clone https://github.com/FredHutch/diy-cromwell-server cromwell`
+# edit 
+
+library(fh.wdlR)
+cromwellCreate(FredHutchId = "pvan", port = "2020",
+               pathToServerLogs = "/fh/fast/gottardo_r/pvan_working/cromwell/cromwell-serverlogs/%A.txt",
+               pathToScript = "/fh/fast/gottardo_r/pvan_working/cromwell/config/cromServer.sh",
+               pathToParams = "/fh/fast/gottardo_r/pvan_working/cromwell/config/cromwellParams.sh")
