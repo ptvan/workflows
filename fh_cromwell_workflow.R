eae0d62d1e02a119f43cef22cc3aa7654ef003f9
ptvan <pvan@fhcrc.org>
2020-02-18 11:20:59 -0800

    more docs

--- fh_cromwell_workflow.R
+++ fh_cromwell_workflow.R
@@ -1,9 +1,16 @@
+# This code sets up a Cromwell workflow at FredHutch using the SciComp infrastructure
+
+
 # 1. Set up MariaDB on gizmo: request a MariaDB instance at https://mydb.fredhutch.org/login
 # note the settings generated, eg. `mysql --host mydb --port 32217 --user pvan --password`
 #
 # 2. Clone the Cromwell server skeleton: 
 # `git clone https://github.com/FredHutch/diy-cromwell-server cromwell`
-# edit cromwellParams.sh as appropriate
+#
+# 3. edit cromwellParams.sh as appropriate, specifically file paths
+# 
+# 4. Check the status of your Cromwell job(s), either by `squeue -u mylogin` or
+# browsing `http://gizmo[nodenumber]:2020`, where 2020 is the port specified below
 
 library(fh.wdlR)
 cromwellCreate(FredHutchId = "pvan", port = "2020",
