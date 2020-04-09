7bf9c47a42d23f0d7be05f988ae614cf81e51a7c
ptvan <pvan@fredhutch.org>
2020-04-09 09:07:56 -0700

    minor updates

--- metagenomics_workflow.sh
+++ metagenomics_workflow.sh
@@ -3,11 +3,19 @@
 # get example FASTA from MAFFT site
 wget https://mafft.cbrc.jp/alignment/software/ex1.txt -O multiDNA.fa
 
-# align sequences using MAFFT
+# align sequences using MAFFT, output a .phy file
 mafft --thread 8 --reorder --treeout multiDNA.fa > multiDNA.phy
-
 
 # construct evolutionary tree using IQ-TREE
 # IQ-TREE takes either a .cf or .phy file 
-# and output a .iqtree file (report) and .treefile file (NEWICK tree)
-iqtree -nt 8 -redo -s example.phy
+# by default IQ-TREE will try to find the correct substitution model
+iqtree -nt 8 -s multiDNA.phy
+
+# looking at multiDNA.phy.iqtree we see that the best model is SYM+R5
+# so on subsequent runs we can specify the model explicitly to save time
+iqtree -nt 8 -s multiDNA.phy -m SYM+R5
+
+# IQ-TREE output a .iqtree file (report) and .treefile file (NEWICK tree)
+
+
+
