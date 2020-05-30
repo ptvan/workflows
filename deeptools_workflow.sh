82017b5636b1a36ebebc066e18ce2fe9025111de
ptvan <pvan@fredhutch.org>
2020-05-30 09:04:26 -0700

    updates

--- deeptools_workflow.sh
+++ deeptools_workflow.sh
@@ -1,6 +1,9 @@
 # create coverage (in the form of a bigWig/bedGraph) file from a BAM file
 bamCoverage -b reads.bam -o coverage.bw --numberOfProcessors 8
 
+# compare ChIP-seq coverages between two different samples
+ multiBamSummary BED-file --BED selection.bed --bamfiles sample1.bam sample2.bam -out results.npz --numberOfProcessors 8
+
 # scale mouse and fly GC contents and plot heatmaps for comparison
 computeMatrix scale-regions -S mouse_GCcontent.bw -R RefSeq_genes_uniq.bed  -m 10000 -b 3000 -a 3000 -out mouse_matrix.tab.gz
 plotHeatmap -m mouse_matrix.tab.gz -out scaledmouseGC.png --colorMap YlGnBu --regionsLabel 'mouse genes'
