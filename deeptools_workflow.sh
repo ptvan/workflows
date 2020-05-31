f6201a1647d3026a2c20b3f39071a8c2f2e06a93
ptvan <pvan@fredhutch.org>
2020-05-31 08:45:00 -0700

    added plotCorrelation example

--- deeptools_workflow.sh
+++ deeptools_workflow.sh
@@ -2,7 +2,15 @@
 bamCoverage -b reads.bam -o coverage.bw --numberOfProcessors 8
 
 # compare ChIP-seq coverages between two different samples
- multiBamSummary BED-file --BED selection.bed --bamfiles sample1.bam sample2.bam -out results.npz --numberOfProcessors 8
+multiBamSummary BED-file --BED selection.bed --bamfiles sample1.bam sample2.bam -out comparison.npz --numberOfProcessors 8
+
+# plotting the results of multiBamSummary
+plotCorrelation -in comparison.npz \
+--corMethod pearson --skipZeros \
+--plotTitle "Pearson Correlation of Average Scores Per Transcript" \
+--whatToPlot scatterplot \
+-o scatterplot_PearsonCorr_bigwigScores.png   \
+--outFileCorMatrix PearsonCorr_bigwigScores.tab
 
 # scale mouse and fly GC contents and plot heatmaps for comparison
 computeMatrix scale-regions -S mouse_GCcontent.bw -R RefSeq_genes_uniq.bed  -m 10000 -b 3000 -a 3000 -out mouse_matrix.tab.gz
