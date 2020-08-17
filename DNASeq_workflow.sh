b49cf0389b28e5b207ba6ce1fab362e7dd116971
ptvan <pvan@fredhutch.org>
2020-08-17 07:54:28 -0700

    finished outline

--- DNASeq_workflow.sh
+++ DNASeq_workflow.sh
@@ -1,3 +1,4 @@
+## https://www.nature.com/articles/s41598-018-25022-6
 # get example FASTQ and BAM files from http://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeUwRepliSeq/
 
 # align to reference
@@ -5,3 +6,10 @@
 
 # sort and index
 
+# mark duplicates and merge
+
+# GATK diagnoseTargets
+
+# call variants
+
+# annotate variants
