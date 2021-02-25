6e958c5aaacb7fc9e6aa354e839f8e2c150e9788
ptvan <nulzilla@gmail.com>
2021-02-25 08:34:27 -0800

    reorg

--- scRNASeq_workflow_scanpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import scanpy as sc
-
-sc.settings.verbosity = 3             
-sc.logging.print_versions()
-sc.settings.set_figure_params(dpi=80, facecolor='white')
