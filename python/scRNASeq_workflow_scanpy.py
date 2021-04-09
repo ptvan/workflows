889f0198810ac08e681e9b9a4879db9a23f4bdbc
ptvan <nulzilla@gmail.com>
2021-04-09 07:28:33 -0700

    updated scanpy

--- python/scRNASeq_workflow_scanpy.py
+++ python/scRNASeq_workflow_scanpy.py
@@ -2,6 +2,19 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 
+# !mkdir data
+# !wget http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz -O data/pbmc3k_filtered_gene_bc_matrices.tar.gz
+# !cd data; tar -xzf pbmc3k_filtered_gene_bc_matrices.tar.gz
+# !mkdir write
+
 sc.settings.verbosity = 3             
 sc.logging.print_versions()
 sc.settings.set_figure_params(dpi=80, facecolor='white')
+
+adata = sc.read_10x_mtx(
+    'data/filtered_gene_bc_matrices/hg19/',  # the directory with the `.mtx` file
+    var_names='gene_symbols',                # use gene symbols for the variable names (variables-axis index)
+    cache=True)   
+
+adata.var_names_make_unique()
+
