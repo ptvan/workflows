e1a2c692e94bf16959a4d14a7c2024ced19c53d0
ptvan <nulzilla@gmail.com>
2021-04-09 07:40:54 -0700

    added analysis to scanpy snippets

--- python/scRNASeq_workflow_scanpy.py
+++ python/scRNASeq_workflow_scanpy.py
@@ -11,10 +11,49 @@ sc.settings.verbosity = 3
 sc.logging.print_versions()
 sc.settings.set_figure_params(dpi=80, facecolor='white')
 
+# load data
 adata = sc.read_10x_mtx(
     'data/filtered_gene_bc_matrices/hg19/',  # the directory with the `.mtx` file
     var_names='gene_symbols',                # use gene symbols for the variable names (variables-axis index)
     cache=True)   
 
 adata.var_names_make_unique()
+
+# plot genes that show highest expression
+sc.pl.highest_expr_genes(adata, n_top=20, )
+
+# flag cells and genes to filter
+sc.pp.filter_cells(adata, min_genes=200)
+sc.pp.filter_genes(adata, min_cells=3)
+
+# annotate the group of mitochondrial genes as 'mt'
+adata.var['mt'] = adata.var_names.str.startswith('MT-')
+
+# calculate metrics
+sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+
+# violin plots
+sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
+             jitter=0.4, multi_panel=True)
+
+# scatter plots
+sc.pl.scatter(adata, x='total_counts', y='pct_counts_mt')
+sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts')
+
+# perform filtering
+adata = adata[adata.obs.n_genes_by_counts < 2500, :]
+adata = adata[adata.obs.pct_counts_mt < 5, :]
+
+# normalize & take log
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
+
+# identify highly-variable genes (HVG)
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+
+# plot HVGs
+sc.pl.highly_variable_genes(adata)
+
+# run PCA
+sc.tl.pca(adata, svd_solver='arpack')
 
