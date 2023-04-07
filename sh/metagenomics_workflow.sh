ddb18e193ed31eceb26c3ef8a89be22adde55d15
ptvan <nulzilla@gmail.com>
2023-04-07 07:31:00 -0700

    updated metagenomics workflow

--- sh/metagenomics_workflow.sh
+++ sh/metagenomics_workflow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 ## get some COVID19 sequences from ViPR
-# https://www.viprbrc.org/brc/home.spg?decorator=corona_ncov    
+# https://www.bv-brc.org/view/Taxonomy/11118#view_tab=genomes    
 
 ## perform a multiple sequence alignment (MSA)
 # using MAFFT, outputting a .phy file
@@ -12,12 +12,12 @@ clustalw COVID19multi.fa
 ## construct evolutionary tree using IQ-TREE
 # IQ-TREE takes either a .cf or .phy file 
 # by default IQ-TREE will try to find the correct substitution model
-# iqtree -nt 8 -s COVID19multi.phy
+iqtree2 -nt 8 -s COVID19multi.phy
 
 # looking at multiDNA.phy.iqtree we see that the best model is SYM+R5
 # so on subsequent runs we can specify the model explicitly to save time
 # IQ-TREE output a .iqtree file (report) and .treefile file (NEWICK tree)
-iqtree -nt 8 -s COVID19multi.phy -m HKY+F
+iqtree2 -nt 8 -s COVID19multi.phy -m HKY+F
 
 ## run treetime, which creates a time-resolved tree from the aligned tree,
 # MSA and dates of sample collection. 
