bc4918b8f79545b7a403b3ace4500fcf23ee9853
ptvan <nulzilla@gmail.com>
2026-05-25 06:49:23 -0700

    minor edits

--- sh/HiC_workflow.sh
+++ sh/HiC_workflow.sh
@@ -10,24 +10,28 @@ samtools view -h output.bam | \
 
 pairtools sort --nproc 8 -o sorted.pairsam.gz parsed.pairsam.gz
 
-# process pairsams: dedup, select, split
+# process pairsams: dedup and select
 pairtools dedup --mark-dup -o deduped.pairsam.gz sorted.pairsam.gz
 pairtools select \
     '(pair_type == "UU") or (pair_type == "UR") or (pair_type == "RU")' \
     -o filtered.pairsam.gz deduped.pairsam.gz
+
+# split pairsam and create pairs file
 pairtools split --output-pairs output.pairs.gz filtered.pairsam.gz
 
-# create pairs file
+# index pairs file
+# NOTE: this operation is in-place and does not produce a new file
 pairix -f output.pairs.gz
 
-# binning
+# binning into a .cool file
 cooler cload pairix hg38.mainonly.chrom.size:500000 output.pairs.gz output.cool
 
-# normalize
-# NOTE: this operation is in-place and does not produce a new file
+# normalize .cool
+# NOTE: in-place operation
 cooler balance output.cool
 
 # aggregate .cool into .mcool
+# NOTE: in-place operation
 cooler zoomify output.cool
 
 # at this point the .mcool file can be view in HiGlass
