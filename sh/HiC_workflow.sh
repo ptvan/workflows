acfaffd8896e5efcea1e75f6a66941241434429b
ptvan <nulzilla@gmail.com>
2026-05-25 07:08:35 -0700

    updates

--- sh/HiC_workflow.sh
+++ sh/HiC_workflow.sh
@@ -5,33 +5,61 @@ bwa mem -SP5M -t8 hg38.fast input_R1.fastq input_R2.fastq | \
    samtools view -bhS - > output.bam
 
 # filter and sort mapped reads
-samtools view -h output.bam | \
-    pairtools parse -c hg38.mainonly.chrom.size -o parsed.pairsam.gz
+pairtools parse -o output.parsed.pairsam.gz -c hg38.fa.sizes \
+  --drop-sam --drop-seq --output-stats output.stats \
+  --assembly hg38 --no-flip \
+  --add-columns mapq \
+  --walks-policy mask \
+  output.bam
 
-pairtools sort --nproc 8 -o sorted.pairsam.gz parsed.pairsam.gz
+pairtools sort --nproc 8 -o output.sorted.pairs.gz output.parsed.pairs.gz
 
-# process pairsams: dedup and select
-pairtools dedup --mark-dup -o deduped.pairsam.gz sorted.pairsam.gz
-pairtools select \
-    '(pair_type == "UU") or (pair_type == "UR") or (pair_type == "RU")' \
-    -o filtered.pairsam.gz deduped.pairsam.gz
+# deduplicate
+pairtools dedup \
+    --max-mismatch 3 \
+    --mark-dups \
+    --output \
+        >( pairtools split \
+            --output-pairs output.nodups.pairs.gz \
+            --output-sam output.nodups.bam \
+         ) \
+    --output-unmapped \
+        >( pairtools split \
+            --output-pairs output.unmapped.pairs.gz \
+            --output-sam output.unmapped.bam \
+         ) \
+    --output-dups \
+        >( pairtools split \
+            --output-pairs output.dups.pairs.gz \
+            --output-sam output.dups.bam \
+         ) \
+    --output-stats output.dedup.stats \
+    output.sorted.pairs.gz
 
-# split pairsam and create pairs file
-pairtools split --output-pairs output.pairs.gz filtered.pairsam.gz
+# select
+pairtools select "mapq1>0 and mapq2>0" output.nodups.pairs.gz -o test.nodups.UU.pairs.gz
 
-# index pairs file
-# NOTE: this operation is in-place and does not produce a new file
-pairix -f output.pairs.gz
+# generate stats
+pairtools stats output.sorted.pairs.gz -o output.stats
+
+# create report from stats file
+multiqc output.stats
 
 # binning into a .cool file
-cooler cload pairix hg38.mainonly.chrom.size:500000 output.pairs.gz output.cool
+cooler cooler cload pairs \
+    -c1 2 -p1 3 -c2 4 -p2 5 \
+    --assembly hg38 \
+    ~/.local/share/genomes/hg38/hg38.fa.sizes:1000000 \ 
+    output.pairs.gz \
+    output.hg38.1000000.cool
 
-# normalize .cool
-# NOTE: in-place operation
-cooler balance output.cool
-
-# aggregate .cool into .mcool
-# NOTE: in-place operation
-cooler zoomify output.cool
+# normalize .cool, aggregate into .mcool, perform balancing on each zoom level
+# NOTE: this is an in-place operation and does not generate a new file
+cooler zoomify \
+    --nproc 5 \
+    --out output.hg38.1000000.mcool \
+    --resolutions 1000000,2000000 \
+    --balance \
+    output.hg38.1000000.cool
 
 # at this point the .mcool file can be view in HiGlass
